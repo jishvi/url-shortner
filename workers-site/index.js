24cbadfa7eb71385ea4c6f85174dcb00235cf165
jishvi <jishnuvasanth@gmail.com>
2021-10-24 00:59:46 +0530

    init

---
+++ workers-site/index.js
@@ -0,0 +1,59 @@
+import { Router } from 'itty-router';
+import { customAlphabet } from 'nanoid';
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
+
+const router = Router();
+const nanoid = customAlphabet(
+  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',
+  6,
+);
+
+router.post('/links', async request => {
+  let slug = nanoid();
+  let requestBody = await request.json();
+  if ('url' in requestBody) {
+    
+    await SHORT.put(slug, requestBody.url, { expirationTtl: 86400 });
+    let shortenedURL = `${new URL(request.url).origin}/${slug}`;
+    let responseBody = {
+      message: 'Link shortened successfully',
+      slug,
+      shortened: shortenedURL,
+    };
+    return new Response(JSON.stringify(responseBody), {
+      headers: { 'content-type': 'application/json' },
+      status: 200,
+    });
+  } else {
+    return new Response("Invalid URL", { status: 400 });
+  }
+});
+
+
+router.get('/:slug', async request => {
+  let link = await SHORT.get(request.params.slug);
+
+  if (link) {
+    return new Response(null, {
+      headers: { Location: link },
+      status: 301,
+    });
+  } else {
+    return new Response('Key not found', {
+      status: 404,
+    });
+  }
+});
+
+async function handleEvent(event) {
+  let requestUrl = new URL(event.request.url);
+  if (requestUrl.pathname === '/' || requestUrl.pathname.includes('static')) {
+    return await getAssetFromKV(event);
+  } else {
+    return await router.handle(event.request);
+  }
+}
+
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event));
+});
